fbeb492fd00d905817e0c2078234001551838d4b
Soham Marik <soham.marik@automationedge.com>
2024-07-17 19:55:52 +0530

    Search

---
+++ opensearch/search.py
@@ -0,0 +1,38 @@
+import boto3
+from constants import COLLECTION_NAME, INDEX_NAME
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+aoss_client = boto3.client("opensearchserverless")
+response = aoss_client.batch_get_collection(names=[COLLECTION_NAME])
+host = response["collectionDetails"][0]["collectionEndpoint"].replace("https://", "")
+
+service = "aoss"
+region = "us-east-1"
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(
+    credentials.access_key,
+    credentials.secret_key,
+    region,
+    service,
+    session_token=credentials.token,
+)
+
+opensearch_client = OpenSearch(
+    hosts=[{"host": host, "port": 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection,
+    timeout=300,
+)
+
+# Search for the document.
+q = "hello"
+query = {
+    "query": {"multi_match": {"query": q, "fields": ["content"]}},
+}
+
+response = opensearch_client.search(body=query, index=INDEX_NAME)
+print("\nSearch results:")
+print(response)

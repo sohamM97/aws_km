49f65be85fb2631b54cb5ea7d50e6fb0796cce81
Soham Marik <soham.marik@automationedge.com>
2024-07-22 17:22:05 +0530

    Create policies and collections using async boto3

--- opensearch/create_collection_and_index.py
+++ opensearch/create_collection_and_index.py
@@ -1,7 +1,7 @@
+import asyncio
 import json
-import time
 
-import boto3
+import aioboto3
 import botocore
 from constants import (
     ACCESS_POLICY_NAME,
@@ -17,40 +17,29 @@ from requests_aws4auth import AWS4Auth
 # You can use the CLI and run 'aws configure' to set access key, secret
 # key, and default region.
 
-client = boto3.client("opensearchserverless")
-service = "aoss"
-region = "us-east-1"
-credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(
-    credentials.access_key,
-    credentials.secret_key,
-    region,
-    service,
-    session_token=credentials.token,
-)
 
-
-def create_encryption_policy(client):
+async def create_encryption_policy(session):
     """Creates an encryption policy that matches all collections beginning with tv-"""
     try:
-        response = client.create_security_policy(
-            description="Encryption policy for TV collections",
-            name=ENCRYPTION_POLICY_NAME,
-            policy=json.dumps(
-                {
-                    "Rules": [
-                        {
-                            "ResourceType": "collection",
-                            "Resource": [f"collection/{COLLECTION_NAME}*"],
-                        }
-                    ],
-                    "AWSOwnedKey": True,
-                }
-            ),
-            type="encryption",
-        )
-        print("\nEncryption policy created:")
-        print(response)
+        async with session.client("opensearchserverless") as client:
+            response = await client.create_security_policy(
+                description="Encryption policy for TV collections",
+                name=ENCRYPTION_POLICY_NAME,
+                policy=json.dumps(
+                    {
+                        "Rules": [
+                            {
+                                "ResourceType": "collection",
+                                "Resource": [f"collection/{COLLECTION_NAME}*"],
+                            }
+                        ],
+                        "AWSOwnedKey": True,
+                    }
+                ),
+                type="encryption",
+            )
+            print("\nEncryption policy created:")
+            print(response)
     except botocore.exceptions.ClientError as error:
         if error.response["Error"]["Code"] == "ConflictException":
             print(
@@ -60,34 +49,35 @@ def create_encryption_policy(client):
             raise error
 
 
-def create_network_policy(client):
+async def create_network_policy(session):
     """Creates a network policy that matches all collections beginning with tv-"""
     try:
-        response = client.create_security_policy(
-            description="Network policy for TV collections",
-            name=NETWORK_POLICY_NAME,
-            policy=json.dumps(
-                [
-                    {
-                        "Description": "Public access for TV collection",
-                        "Rules": [
-                            {
-                                "ResourceType": "dashboard",
-                                "Resource": [f"collection/{COLLECTION_NAME}*"],
-                            },
-                            {
-                                "ResourceType": "collection",
-                                "Resource": [f"collection/{COLLECTION_NAME}*"],
-                            },
-                        ],
-                        "AllowFromPublic": True,
-                    }
-                ]
-            ),
-            type="network",
-        )
-        print("\nNetwork policy created:")
-        print(response)
+        async with session.client("opensearchserverless") as client:
+            response = await client.create_security_policy(
+                description="Network policy for TV collections",
+                name=NETWORK_POLICY_NAME,
+                policy=json.dumps(
+                    [
+                        {
+                            "Description": "Public access for TV collection",
+                            "Rules": [
+                                {
+                                    "ResourceType": "dashboard",
+                                    "Resource": [f"collection/{COLLECTION_NAME}*"],
+                                },
+                                {
+                                    "ResourceType": "collection",
+                                    "Resource": [f"collection/{COLLECTION_NAME}*"],
+                                },
+                            ],
+                            "AllowFromPublic": True,
+                        }
+                    ]
+                ),
+                type="network",
+            )
+            print("\nNetwork policy created:")
+            print(response)
     except botocore.exceptions.ClientError as error:
         if error.response["Error"]["Code"] == "ConflictException":
             print("[ConflictException] A network policy with this name already exists.")
@@ -95,43 +85,44 @@ def create_network_policy(client):
             raise error
 
 
-def create_access_policy(client):
+async def create_access_policy(session):
     """Creates a data access policy that matches all collections beginning with tv-"""
     try:
-        response = client.create_access_policy(
-            description="Data access policy for TV collections",
-            name=ACCESS_POLICY_NAME,
-            # TODO: principal name is hardcoded
-            policy=json.dumps(
-                [
-                    {
-                        "Rules": [
-                            {
-                                "Resource": [f"index/{INDEX_NAME}*/*"],
-                                "Permission": [
-                                    "aoss:CreateIndex",
-                                    "aoss:DeleteIndex",
-                                    "aoss:UpdateIndex",
-                                    "aoss:DescribeIndex",
-                                    "aoss:ReadDocument",
-                                    "aoss:WriteDocument",
-                                ],
-                                "ResourceType": "index",
-                            },
-                            {
-                                "Resource": [f"collection/{COLLECTION_NAME}*"],
-                                "Permission": ["aoss:CreateCollectionItems"],
-                                "ResourceType": "collection",
-                            },
-                        ],
-                        "Principal": ["arn:aws:iam::514857968326:user/dash"],
-                    }
-                ]
-            ),
-            type="data",
-        )
-        print("\nAccess policy created:")
-        print(response)
+        async with session.client("opensearchserverless") as client:
+            response = await client.create_access_policy(
+                description="Data access policy for TV collections",
+                name=ACCESS_POLICY_NAME,
+                # TODO: principal name is hardcoded
+                policy=json.dumps(
+                    [
+                        {
+                            "Rules": [
+                                {
+                                    "Resource": [f"index/{INDEX_NAME}*/*"],
+                                    "Permission": [
+                                        "aoss:CreateIndex",
+                                        "aoss:DeleteIndex",
+                                        "aoss:UpdateIndex",
+                                        "aoss:DescribeIndex",
+                                        "aoss:ReadDocument",
+                                        "aoss:WriteDocument",
+                                    ],
+                                    "ResourceType": "index",
+                                },
+                                {
+                                    "Resource": [f"collection/{COLLECTION_NAME}*"],
+                                    "Permission": ["aoss:CreateCollectionItems"],
+                                    "ResourceType": "collection",
+                                },
+                            ],
+                            "Principal": ["arn:aws:iam::514857968326:user/dash"],
+                        }
+                    ]
+                ),
+                type="data",
+            )
+            print("\nAccess policy created:")
+            print(response)
     except botocore.exceptions.ClientError as error:
         if error.response["Error"]["Code"] == "ConflictException":
             print("[ConflictException] An access policy with this name already exists.")
@@ -139,11 +130,14 @@ def create_access_policy(client):
             raise error
 
 
-def create_collection(client):
+async def create_collection(session):
     """Creates a collection"""
     try:
-        response = client.create_collection(name=COLLECTION_NAME, type="VECTORSEARCH")
-        return response
+        async with session.client("opensearchserverless") as client:
+            response = await client.create_collection(
+                name=COLLECTION_NAME, type="VECTORSEARCH"
+            )
+            return response
     except botocore.exceptions.ClientError as error:
         if error.response["Error"]["Code"] == "ConflictException":
             print(
@@ -153,23 +147,24 @@ def create_collection(client):
             raise error
 
 
-def wait_for_collection_creation(client):
+async def wait_for_collection_creation(session, awsauth):
     """Waits for the collection to become active"""
-    response = client.batch_get_collection(names=[COLLECTION_NAME])
-    # Periodically check collection status
-    while (response["collectionDetails"][0]["status"]) == "CREATING":
-        print("Creating collection...")
-        time.sleep(30)
-        response = client.batch_get_collection(names=[COLLECTION_NAME])
-    print("\nCollection successfully created:")
-    print(response["collectionDetails"])
-    # Extract the collection endpoint from the response
-    host = response["collectionDetails"][0]["collectionEndpoint"]
-    final_host = host.replace("https://", "")
-    index_data(final_host)
+    async with session.client("opensearchserverless") as client:
+        response = await client.batch_get_collection(names=[COLLECTION_NAME])
+        # Periodically check collection status
+        while (response["collectionDetails"][0]["status"]) == "CREATING":
+            print("Creating collection...")
+            await asyncio.sleep(30)
+            response = await client.batch_get_collection(names=[COLLECTION_NAME])
+        print("\nCollection successfully created:")
+        print(response["collectionDetails"])
+        # Extract the collection endpoint from the response
+        host = response["collectionDetails"][0]["collectionEndpoint"]
+        final_host = host.replace("https://", "")
+        await index_data(final_host, awsauth)
 
 
-def index_data(host):
+async def index_data(host, awsauth):
     """Create an index and add some sample data"""
     # Build the OpenSearch client
     client = OpenSearch(
@@ -181,50 +176,64 @@ def index_data(host):
         timeout=300,
     )
     # It can take up to a minute for data access rules to be enforced
-    time.sleep(45)
+    # await asyncio.sleep(45)
 
     # Create index
-    response = client.indices.create(
-        index=INDEX_NAME,
-        body={
-            "settings": {"index.knn": True},
-            "mappings": {
-                "properties": {
-                    "embedding": {
-                        "type": "knn_vector",
-                        # TODO: Change to 1536
-                        "dimension": 3,
-                        "method": {
-                            "engine": "nmslib",
-                            "name": "hnsw",
-                            "space_type": "cosinesimil",
+    if client.indices.exists(index=INDEX_NAME):
+        print(f"Index {INDEX_NAME} already exists!")
+    else:
+        response = client.indices.create(
+            index=INDEX_NAME,
+            body={
+                "settings": {"index.knn": True},
+                "mappings": {
+                    "properties": {
+                        "embedding": {
+                            "type": "knn_vector",
+                            "dimension": 3,
+                            "method": {
+                                "engine": "nmslib",
+                                "name": "hnsw",
+                                "space_type": "cosinesimil",
+                            },
                         },
-                    },
-                    # TODO: do we need id? AWS generates its own _id, check how to use
-                    # that
-                    "id": {"type": "text"},
-                    "project_uuid": {"type": "text"},
-                    "filename": {"type": "text"},
-                    "content": {"type": "text"},
-                    "sourcepage": {"type": "text"},
-                    "sourcefilepath": {"type": "text"},
-                    "language": {"type": "text"},
-                    "tags": {"type": "long"},
-                }
+                        # TODO: do we need id? AWS generates its own _id, check how to use
+                        # that
+                        "id": {"type": "text"},
+                        "project_uuid": {"type": "text"},
+                        "filename": {"type": "text"},
+                        "content": {"type": "text"},
+                        "sourcepage": {"type": "text"},
+                        "sourcefilepath": {"type": "text"},
+                        "language": {"type": "text"},
+                        "tags": {"type": "long"},
+                    }
+                },
             },
-        },
+        )
+        print("\nCreating index:")
+        print(response)
+
+
+async def main():
+    session = aioboto3.Session()
+    service = "aoss"
+    region = "us-east-1"
+    credentials = await session.get_credentials()
+    awsauth = AWS4Auth(
+        credentials.access_key,
+        credentials.secret_key,
+        region,
+        service,
+        security_token=credentials.token,
     )
-    print("\nCreating index:")
-    print(response)
 
-
-def main():
-    create_encryption_policy(client)
-    create_network_policy(client)
-    create_access_policy(client)
-    create_collection(client)
-    wait_for_collection_creation(client)
+    await create_encryption_policy(session)
+    await create_network_policy(session)
+    await create_access_policy(session)
+    await create_collection(session)
+    await wait_for_collection_creation(session, awsauth)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())

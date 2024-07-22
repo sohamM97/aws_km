8298845b1b2a82e396473ce327aca665b09f1e2b
Soham Marik <soham.marik@automationedge.com>
2024-07-22 18:53:33 +0530

    Async upload and search

--- opensearch/search.py
+++ opensearch/search.py
@@ -1,46 +1,67 @@
-import boto3
+import asyncio
+import os
+
+import aioboto3
 from constants import COLLECTION_NAME, INDEX_NAME
-from opensearchpy import OpenSearch, RequestsHttpConnection
-from requests_aws4auth import AWS4Auth
+from dotenv import load_dotenv
+from opensearchpy import AsyncHttpConnection, AsyncOpenSearch, AWSV4SignerAsyncAuth
 
-aoss_client = boto3.client("opensearchserverless")
-response = aoss_client.batch_get_collection(names=[COLLECTION_NAME])
-host = response["collectionDetails"][0]["collectionEndpoint"].replace("https://", "")
+load_dotenv()
 
-service = "aoss"
-region = "us-east-1"
-credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(
-    credentials.access_key,
-    credentials.secret_key,
-    region,
-    service,
-    session_token=credentials.token,
-)
 
-opensearch_client = OpenSearch(
-    hosts=[{"host": host, "port": 443}],
-    http_auth=awsauth,
-    use_ssl=True,
-    verify_certs=True,
-    connection_class=RequestsHttpConnection,
-    timeout=300,
-)
+async def main():
 
-# Search for the document.
-q = "dhoni"
-query = {
-    "query": {
-        "bool": {
-            "must": [
-                {"knn": {"embedding": {"vector": [1, 2, 3], "k": 50}}},
-                {"multi_match": {"query": q, "fields": ["content"]}},
-            ],
-            "filter": [{"terms": {"tags": [1, 2]}}],
-        }
-    },
-}
+    session = aioboto3.Session(
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
+    )
 
-response = opensearch_client.search(body=query, index=INDEX_NAME)
-print("\nSearch results:")
-print(response)
+    async with session.client("opensearchserverless") as aoss_client:
+        response = await aoss_client.batch_get_collection(names=[COLLECTION_NAME])
+
+    host = response["collectionDetails"][0]["collectionEndpoint"].replace(
+        "https://", ""
+    )
+
+    service = "aoss"
+    region = "us-east-1"
+    credentials = await session.get_credentials()
+    credentials = await credentials.get_frozen_credentials()
+    awsauth = AWSV4SignerAsyncAuth(
+        credentials,
+        region,
+        service,
+    )
+
+    opensearch_client = AsyncOpenSearch(
+        hosts=[{"host": host, "port": 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=AsyncHttpConnection,
+        timeout=300,
+    )
+
+    # Search for the document.
+    q = "dhoni"
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"knn": {"embedding": {"vector": [1, 2, 3], "k": 50}}},
+                    {"multi_match": {"query": q, "fields": ["content"]}},
+                ],
+                "filter": [{"terms": {"tags": [1, 2]}}],
+            }
+        },
+    }
+
+    response = await opensearch_client.search(body=query, index=INDEX_NAME)
+    print("\nSearch results:")
+    print(response)
+
+    await opensearch_client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- opensearch/upload_and_index_document.py
+++ opensearch/upload_and_index_document.py
@@ -1,65 +1,83 @@
+import asyncio
+import os
 from uuid import uuid4
 
-import boto3
+import aioboto3
 from constants import COLLECTION_NAME, INDEX_NAME
-from opensearchpy import OpenSearch, RequestsHttpConnection
+from dotenv import load_dotenv
+from opensearchpy import AsyncHttpConnection, AsyncOpenSearch, AWSV4SignerAsyncAuth
 from pypdf import PdfReader
-from requests_aws4auth import AWS4Auth
 from upload import BUCKET_NAME, upload_file
+
+load_dotenv()
 
 FILE_NAME = "msdhoni.pdf"
 
 
-s3_url = upload_file(file_name=FILE_NAME, bucket=BUCKET_NAME)
+async def main():
 
-service = "aoss"
-region = "us-east-1"
-credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(
-    credentials.access_key,
-    credentials.secret_key,
-    region,
-    service,
-    session_token=credentials.token,
-)
+    s3_url = await upload_file(file_name=FILE_NAME, bucket=BUCKET_NAME)
+    session = aioboto3.Session(
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
+    )
 
-aoss_client = boto3.client("opensearchserverless")
-response = aoss_client.batch_get_collection(names=[COLLECTION_NAME])
-host = response["collectionDetails"][0]["collectionEndpoint"].replace("https://", "")
+    service = "aoss"
+    region = "us-east-1"
+    credentials = await session.get_credentials()
+    credentials = await credentials.get_frozen_credentials()
+    awsauth = AWSV4SignerAsyncAuth(
+        credentials,
+        region,
+        service,
+    )
 
-# Build the OpenSearch client
-opensearch_client = OpenSearch(
-    hosts=[{"host": host, "port": 443}],
-    http_auth=awsauth,
-    use_ssl=True,
-    verify_certs=True,
-    connection_class=RequestsHttpConnection,
-    timeout=300,
-)
+    async with session.client("opensearchserverless") as aoss_client:
+        response = await aoss_client.batch_get_collection(names=[COLLECTION_NAME])
 
-if FILE_NAME.endswith(".pdf"):
-    content = ""
-    reader = PdfReader(FILE_NAME)
-    for i in range(len(reader.pages)):
-        page = reader.pages[i]
-        content += page.extract_text()
-else:
-    with open(FILE_NAME) as f:
-        content = f.read()
+    host = response["collectionDetails"][0]["collectionEndpoint"].replace(
+        "https://", ""
+    )
 
-response = opensearch_client.index(
-    index=INDEX_NAME,
-    body={
-        "id": uuid4(),
-        "project_uuid": uuid4(),
-        "filename": FILE_NAME,
-        "content": content,
-        "sourcepage": FILE_NAME,
-        "sourcefilepath": s3_url,
-        "language": "english",
-        "tags": [1, 2, 3],
-        "embedding": [1, 2, 3],
-    },
-)
-print("\nDocument added:")
-print(response)
+    # Build the OpenSearch client
+    opensearch_client = AsyncOpenSearch(
+        hosts=[{"host": host, "port": 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=AsyncHttpConnection,
+        timeout=300,
+    )
+
+    if FILE_NAME.endswith(".pdf"):
+        content = ""
+        reader = PdfReader(FILE_NAME)
+        for i in range(len(reader.pages)):
+            page = reader.pages[i]
+            content += page.extract_text()
+    else:
+        with open(FILE_NAME) as f:
+            content = f.read()
+
+    response = await opensearch_client.index(
+        index=INDEX_NAME,
+        body={
+            "id": uuid4(),
+            "project_uuid": uuid4(),
+            "filename": FILE_NAME,
+            "content": content,
+            "sourcepage": FILE_NAME,
+            "sourcefilepath": s3_url,
+            "language": "english",
+            "tags": [1, 2, 3],
+            "embedding": [1, 2, 3],
+        },
+    )
+    print("\nDocument added:")
+    print(response)
+
+    await opensearch_client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

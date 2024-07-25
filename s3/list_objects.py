11f1f0e52ef1daa4790739ce01608c3cd2158090
Soham Marik <soham.marik@automationedge.com>
2024-07-25 17:14:53 +0530

    List objects under bucket

---
+++ s3/list_objects.py
@@ -0,0 +1,21 @@
+import asyncio
+import os
+
+import aioboto3
+
+BUCKET_NAME = "soham-boto-s3-test"
+
+
+async def main():
+    s3_session = aioboto3.Session(
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
+    )
+    async with s3_session.client("s3") as s3_client:
+        objects = await s3_client.list_objects_v2(Bucket=BUCKET_NAME)
+        for obj in objects["Contents"]:
+            print(obj["Key"])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
